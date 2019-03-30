bcc25c72f8842ea0ea971a2c4e51593571b8f336
Alper Çaktuğ <alper.caktug@ogr.deu.edu.tr>
2019-03-30 14:03:37 +0300

    Proje iskeleti oluşturuldu.

---
+++ Proje1/AI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1
+{
+    //Sözcük seçer ve oyuncu sınıfından gelecek tahminlere cevap verir.
+    class AI
+    {
+    }
+}
